8c590a0f25bff9fa617380267927b39109a1c834
playerzer0-ui <playerzero745@gmail.com>
2023-11-30 16:48:44 +0000

    add book, genre and some dependency. Also add sql database

---
+++ src/main/java/business/Book.java
@@ -0,0 +1,95 @@
+package business;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private int bookID;
+    private String bookName;
+    private String author;
+    private String description;
+    private int quantity;
+
+    public Book() {
+    }
+
+    public Book(int bookID, String bookName, String author, String description, int quantity) {
+        this.bookID = bookID;
+        this.bookName = bookName;
+        this.author = author;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public Book(String bookName, String author, String description, int quantity) {
+        this.bookName = bookName;
+        this.author = author;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return bookID == book.bookID;
+    }
+
+    @Override
+    public int hashCode() {
+        return bookID;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookID=" + bookID +
+                ", bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
---
+++ src/main/java/business/Genre.java
@@ -0,0 +1,56 @@
+package business;
+
+import java.io.Serializable;
+
+public class Genre implements Serializable {
+    private int genreID;
+    private String genreName;
+
+
+    public Genre() {
+    }
+
+    public Genre(int genreID, String genreName) {
+        this.genreID = genreID;
+        this.genreName = genreName;
+    }
+
+    public int getGenreID() {
+        return genreID;
+    }
+
+    public void setGenreID(int genreID) {
+        this.genreID = genreID;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Genre genre = (Genre) o;
+
+        return genreID == genre.genreID;
+    }
+
+    @Override
+    public int hashCode() {
+        return genreID;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "genreID=" + genreID +
+                ", genreName='" + genreName + '\'' +
+                '}';
+    }
+}

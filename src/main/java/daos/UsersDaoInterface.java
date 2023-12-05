e2da1495c8ec5b37bf5cd8a43aa2cef600905f0e
Mercy <d00251752@student.dkit.ie>
2023-12-05 23:56:02 +0000

    Add deleteUser() and amendUser() in UserDaoInterface.

--- src/main/java/daos/UsersDaoInterface.java
+++ src/main/java/daos/UsersDaoInterface.java
@@ -9,4 +9,7 @@ public interface UsersDaoInterface {
     public Users findUserByUsernamePassword(String uname, String pword);
     public Users findUserById(int id);
     public int addUser(String uname,String email, String pword, String address, String phone, int userType);
+    public int deleteUser(int userId);
+    public int amendUser(Users u) ;
+
 }

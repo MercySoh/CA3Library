8b1cb28f9c18c424371879bad758633b47b2c706
d00234340 <d00234340@student.dkit.ie>
2023-12-11 10:54:57 +0000

    commit Controller and add some test.jsp

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -12,22 +12,30 @@ public class Controller extends HttpServlet {
         message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         processRequest(request, response);
     }
 
-    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        processRequest(request, response);
+    }
 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String action = request.getParameter("action");
+        String dest = "index.jsp";
+
+        if(action != null){
+            switch (action){
+                case "test":
+                    dest = "test.jsp";
+                    break;
+
+
+            }
+        }
+        response.sendRedirect(dest);
     }
 
     public void destroy() {

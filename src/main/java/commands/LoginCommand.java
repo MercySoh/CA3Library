cf16158e6ac0abcf70045a79c2d0f7595ec529f0
playerzer0-ui <playerzero745@gmail.com>
2023-12-19 08:18:04 +0000

    the commands are filled

--- src/main/java/commands/LoginCommand.java
+++ src/main/java/commands/LoginCommand.java
@@ -1,4 +1,37 @@
 package commands;
 
-public class LoginCommand {
+import business.Users;
+import daos.UsersDao;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginCommand implements Command{
+
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    public LoginCommand(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+    }
+    @Override
+    public String execute() {
+        HttpSession session = request.getSession(true);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UsersDao userDao = new UsersDao("ca3library");
+        Users user = userDao.findUserByUsernamePassword(username, password);
+
+        if(user != null){
+            session.setAttribute("user", user);
+            return "dashboard.jsp";
+        }
+        else{
+            String msg = "Wrong password or UserName";
+            session.setAttribute("msg", msg);
+            return "login.jsp";
+        }
+    }
 }
--- src/main/java/commands/LogoutCommand.java
+++ src/main/java/commands/LogoutCommand.java
@@ -1,4 +1,24 @@
 package commands;
 
-public class LogoutCommand {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LogoutCommand implements Command{
+
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    public LogoutCommand(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+    }
+
+
+    @Override
+    public String execute() {
+        HttpSession session = request.getSession(true);
+        session.invalidate();
+        return "index.jsp";
+    }
 }
